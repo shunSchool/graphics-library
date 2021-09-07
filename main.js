d0845554d27fad6483fd13aa989615065d64403f
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-07 11:31:34 -0600

    started actually doing stuff

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ let ctx = cnv.msGetInputContext("2d");
 cnv.width = 800;
 cnv.height = 600;
 
+//draw a line
+ctx.strokeStyle = "blue:";
+ctx.beginPath();
+ctx.moveTo(100, 200); //Endpoint 1
+ctx.lineTo(300, 100); //endpoint 2
+ctx.stroke();
