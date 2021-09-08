53a4ca245ed548f69aa3d5d83ffceb5b7399d169
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-08 11:09:34 -0600

    line

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ cnv.height = 600;
 //draw a line
 ctx.strokeStyle = "blue:";
 ctx.lineWidth = 3;
-line();
-ctx.beginPath();
-ctx.moveTo(100, 200); //Endpoint 1
-ctx.lineTo(300, 100); //endpoint 2
-ctx.stroke();
+line(100, 200, 300, 100);
 
+ctx.strokeStyle = "red";
+line(500, 50, 300, 450);
+line(0, 0, 100, 100);
 
-function line() {
-    console.log("line");
+//draw a line segment from (x1, y1) to (x2, y2)
+function line(x1, y1, x2, y2) {
+    console.log(x1, y1, x2, y2);
     ctx.beginPath();
-    ctx.moveTo(100, 200); //Endpoint 1
-    ctx.lineTo(300, 100); //endpoint 2
+    ctx.moveTo(x1, y1); //Endpoint 1
+    ctx.lineTo(x2, y2); //endpoint 2
     ctx.stroke();    
 }
