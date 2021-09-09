cdc8539a8b8b83c7a839b0458b81d5f6d5a86184
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-09 11:04:37 -0600

    rectangle done

---
+++ graphics library.js
@@ -0,0 +1,17 @@
+//draw a stroke or fill rectangle with a top left vorner of x, y a width of w and a height of h
+function rect(x, y, w, h, mode) {
+    if (mode === "fill"){
+   ctx.fillRect(x, y, w, h);
+    } else if(mode === "stroke"){
+        ctx.strokeRect(x, y, w,h);
+    }
+   }
+   
+   //draw a line segment from (x1, y1) to (x2, y2)
+   function line(x1, y1, x2, y2) {
+       console.log(x1, y1, x2, y2);
+       ctx.beginPath();
+       ctx.moveTo(x1, y1); //Endpoint 1
+       ctx.lineTo(x2, y2); //endpoint 2
+       ctx.stroke();    
+   }
--- main.js
+++ main.js
@@ -14,19 +14,4 @@ line(100, 200, 300, 100);
 ctx.fillStyle = "orange";
 rect(100, 100, 200, 50, "fill");
 rect(400, 500, 70, 100, "stroke");
-function rect(x, y, w, h, mode) {
- if (mode === "fill"){
-ctx.fillRect(x, y, w, h);
- } else if(mode === "stroke"){
-     ctx.strokeRect(x, y, w,h);
- }
-}
 
-//draw a line segment from (x1, y1) to (x2, y2)
-function line(x1, y1, x2, y2) {
-    console.log(x1, y1, x2, y2);
-    ctx.beginPath();
-    ctx.moveTo(x1, y1); //Endpoint 1
-    ctx.lineTo(x2, y2); //endpoint 2
-    ctx.stroke();    
-}
