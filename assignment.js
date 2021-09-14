db434d9cbea0a981d72c1e14bbdbdae1e7a08b12
shunSchool <72032942+shunSchool@users.noreply.github.com>
2021-09-14 11:32:23 -0600

    assignment 2

--- assignment.js
+++ assignment.js
@@ -4,13 +4,23 @@ cnv.width = 800;
 cnv.height = 600;
 
 //drawmogus
-function drawmogus (x, y, color, color) {
+function drawmogus (x, y,) {
     //draw astronaut with top left corner (X, Y)
-    //with given color
-    rect(200, 200, 40, 60, "fill"); //body
-    circle(220, 200, 20, "fill"); //head
-    rect(190, 210, 10, 40, "fill"); // backpack
-    rect(220, 220, 30, 20, "fill2"); //visor
+    //with the given colors
+    fill("red");
+    rect(x + 0, y + 20, 40, 60, "fill"); //body
+    circle(x + 20, y + 20, 20, "fill"); //head
+    rect(x - 10, y + 25, 10, 40, "fill"); // backpack
+    fill("blue");
+    rect(x + 20, y + 20, 30, 20, "fill"); //visor
+    fill("red");
+    rect(x + 0, y + 80, 14, 14, "fill"); //left leg
+    rect(x + 26, y + 80, 14, 14, "fill"); //right leg
 }
-
-drawmogus (20, 20, fill("red"), fill2("blue"))
+function drawarrow (x1, y1, x2, y2){
+    lineWidth(3)
+    line(0, 0, 20, 20);
+    triangle(10, 40,)
+}
+drawmogus (200, 200,);
+drawarrow (300,400);
